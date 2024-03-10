8c7e98b8168d52e68d6eb982688dbb3c06d8bb36
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-10 16:49:51 +0100

    Feat index.php - print title-language-rating with html

--- index.php
+++ index.php
@@ -6,7 +6,11 @@ class Production {
     public $language;
     public $rating;
 
-    function __construct($title, $language, $rating) {
+    function __construct(
+        string $title, 
+        string $language, 
+        int $rating
+        ) {
         $this->title = $title;
         $this->language = $language;
         $this->rating = $rating;
@@ -24,5 +28,34 @@ class Production {
 }
 
 
-$film = new Production("Film", "Language", 5);
-var_dump($film);
+$film = new Production("Film", "EN", 7);
+$serie_TV = new Production("Serie TV", "FR", 8);
+
+/* var_dump($serie_TV);
+var_dump($film); */
+
+$productions = [
+    $film,
+    $serie_TV
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php foreach ($productions as $production): ?>
+        <ul>
+            <li>Title: <?= $production->title ?></li>
+            <li>Language: <?= $production->language ?></li>
+            <li>Rating: <?= $production->rating ?></li>
+        </ul>
+
+    <?php endforeach; ?>
+</body>
+</html>
