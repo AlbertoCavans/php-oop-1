647b2b8ba1fdb80f29262250fa509564a6a87b85
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-10 19:40:15 +0100

    Feat index.php - basic layout

--- index.php
+++ index.php
@@ -12,9 +12,16 @@ require_once __DIR__ ."/DB/productions.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
+    <h1 class="m-4">Movies & TVSeries cards</h1>
+<div class="my-4 container d-flex justify-content-center">
+
     <?php foreach ($productions as $production): ?>
+        <div class="m-3 card w-25">
         <ul>
             <li>Title: <?= $production->title ?></li>
             <li>Language: <?= $production->language ?></li>
@@ -25,7 +32,10 @@ require_once __DIR__ ."/DB/productions.php";
             <li>Duration: <?= $production->movie->duration ?></li>
             <li>Number seasons: <?= $production->tvserie->nseasons ?></li>
         </ul>
+        </div>
+      
 
     <?php endforeach; ?>
+    </div>
 </body>
 </html>
